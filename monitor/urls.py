a83a9e42b2fe6b27713a4dad936323739d55b648
odilbeknur <odilbeknur@gmail.com>
2024-07-22 12:08:05 +0500

    update

--- monitor/urls.py
+++ monitor/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import dashboard
+from .views import dashboard , fetch_data_from_api
 
 urlpatterns = [
-    path('monitor/', dashboard, name='dashboard'),  # Root URL for 'main/'
+    path('monitor/', dashboard, name='dashboard'), 
+     path('api/data/', fetch_data_from_api, name='fetch_data_from_api'),
 ]
--- monitor/views.py
+++ monitor/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 import requests
 from django.http import JsonResponse
+from datetime import datetime, timedelta
+from collections import defaultdict
 
 # Create your views here.
 
@@ -16,16 +18,31 @@ def dashboard(request):
 #     return render(request, 'admin/admin-base.html', {'query': query, 'get_cat': get_cat})
 def fetch_data_from_api(request):
     # Replace with your API URL
-    api_url = "https://api.example.com/data"
+    api_url = "http://10.40.9.46:8080/date"
     response = requests.get(api_url)
     data = response.json()
     
     # Optionally process the data if needed
     processed_data = process_data(data)
-    
+    print(processed_data)
     return JsonResponse(processed_data, safe=False)
 
 def process_data(data):
-    # Process data as needed for Chart.js
-    # Example: Convert to required format
-    return data
+        
+    specific_date = datetime(2024, 7, 21)
+
+    time_values = [
+    datetime.strptime(entry["create_date"], "%Y-%m-%dT%H:%M:%S.%f").strftime("%H:%M")
+    for entry in data
+    if entry["serial_number"] == "XKK9CLS03V" and
+       datetime.strptime(entry["create_date"], "%Y-%m-%dT%H:%M:%S.%f").date() == specific_date.date()
+    ]
+
+
+    power_values = [
+    int(float(entry["inverter_registers_date"]["Current-power"]["date"]))
+    for entry in data
+    if entry["serial_number"] == "XKK9CLS03V" and
+       datetime.strptime(entry["create_date"], "%Y-%m-%dT%H:%M:%S.%f").date() == specific_date.date()
+    ]
+    return {"data": power_values, "labels": time_values}
